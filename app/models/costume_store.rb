4afbaf643e79f0f748424af66c13f648436dd0d7
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-06-17 10:15:51 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,13 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base
+	def opening_time
+		d = Date.today
+  		Time.new(d.year, d.month, d.day, 9, 00)
+	end
+
+	def closing_time
+		d = Date.today
+  		Time.new(d.year, d.month, d.day, 22, 00)
+  	end
+end 
